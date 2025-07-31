51f8562a190d01d01843e32a878849c6402032ce
Emile Badenhorst <myvarhd@gmail.com>
2025-07-31 12:52:27 +0200

    Init

---
+++ ssg.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+original_dir="$(pwd)"
+cd "$(dirname "$0")"
+
+exe_name=ssg
+
+# build
+rm $exe_name 2>/dev/null
+
+command="$exe_name.c -std=c89 -o $exe_name -g"
+bear -- gcc $command
+gcc $command
+
+# run
+if [ -e "./$exe_name" ]; then
+  cd "$original_dir"
+  "$(dirname "$0")/$exe_name" "${@:1}"
+fi
